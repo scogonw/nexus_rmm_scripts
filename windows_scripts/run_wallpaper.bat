33eb590874f187ed65a64071c39b27c62e63faee
ksingh-scogo <karan@scogo.in>
2025-03-02 16:39:03 +0530

    updates to script

--- windows_scripts/run_wallpaper.bat
+++ windows_scripts/run_wallpaper.bat
@@ -8,5 +8,6 @@ set "PS_URL=https://raw.githubusercontent.com/scogonw/nexus_rmm_scripts/refs/hea
 set "PS_SCRIPT=%TEMP%\set_wallpaper.ps1"
 
 powershell -Command "& { Invoke-WebRequest -Uri '%PS_URL%' -OutFile '%PS_SCRIPT%' }"
+echo PowerShell script downloaded to: %PS_SCRIPT%
 powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -Command ""& ''%PS_SCRIPT%'' -ImageFile ''%~1''"" ' -Verb RunAs"
-del "%PS_SCRIPT%" 2>nul
+pause
