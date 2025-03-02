c2efd468f428f7bc96e9a17936de0515b92ccbf4
ksingh-scogo <karan@scogo.in>
2025-03-02 16:32:54 +0530

    updates to script

--- windows_scripts/run_wallpaper.bat
+++ windows_scripts/run_wallpaper.bat
@@ -1,11 +1,6 @@
 @echo off
-SETLOCAL EnableDelayedExpansion
-
 if "%~1"=="" (
-    echo ERROR: No image file specified.
-    echo Usage: %~nx0 [path_to_image] [style]
-    echo Example: %~nx0 C:\wallpaper.jpg Stretch
-    echo Available styles: Fill, Fit, Stretch, Tile, Center, Span (default)
+    echo Usage: %~nx0 [path_to_image]
     exit /b 1
 )
 
@@ -13,18 +8,5 @@ set "PS_URL=https://raw.githubusercontent.com/scogonw/nexus_rmm_scripts/refs/hea
 set "PS_SCRIPT=%TEMP%\set_wallpaper.ps1"
 
 powershell -Command "& { Invoke-WebRequest -Uri '%PS_URL%' -OutFile '%PS_SCRIPT%' }"
-
-if not exist "%PS_SCRIPT%" (
-    echo ERROR: Failed to download the PowerShell script.
-    exit /b 1
-)
-
-if "%~2"=="" (
-    powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -Command ""& ''%PS_SCRIPT%'' -ImageFile ''%~1''"" ' -Verb RunAs"
-) else (
-    powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -Command ""& ''%PS_SCRIPT%'' -ImageFile ''%~1'' -Style ''%~2''"" ' -Verb RunAs"
-)
-
-timeout /t 2 > nul
+powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -Command ""& ''%PS_SCRIPT%'' -ImageFile ''%~1''"" ' -Verb RunAs"
 del "%PS_SCRIPT%" 2>nul
-exit /b 0
